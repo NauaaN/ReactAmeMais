f083b14b42eb9ac9ea9d7bb2496b5fc300a5d5b7
eliza <elizaxavier2@gmail.com>
2022-06-11 23:47:41 -0300

    cadastra pet quase pronta 50%

--- src/pages/cadastrarpet/index.js
+++ src/pages/cadastrarpet/index.js
@@ -1,16 +1,47 @@
 import './index.scss';
 import { Link } from 'react-router-dom';
 import '../../common/common.scss';
+import { useState } from 'react';
+import { cadastrarPet, enviarImagemPet} from '../../api/petAPi'
+import storage from 'local-storage'
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
 
-export default function index() {
+export default function Index() {
+    const [animal, setAnimal] = useState('');
+    const [especie, setEspecie] = useState('');
+    const [nome, setNome] = useState('');
+    const [genero, setGenero] = useState('');
+    const [idade, setIdade] = useState('');
+    const [peso, setPeso] = useState('');
+    const [altura, setAltura] = useState('');
+    const [telefone, setTelefone ] = useState('');
+    const [endereco, setEndereco] = useState('');
+    const [comentario, setComentario] = useState('');
+    const [imagem, setImagem] = useState('');
 
+
+    
+   async function salvarClick(){
+        try{
+            const usuario = storage('usuario-logado').id;
+            const r = await cadastrarPet(animal, especie, nome, genero, idade, peso, altura, telefone, endereco, comentario, usuario );
+  
+            toast.error('Pet cadastrado com sucesso!');
+            
+        } catch (err){
+            toast.error(err.message);
+        }
+    }
+    
     return(
      <div className='fundox'>
-<header className='faixav'>
+          <ToastContainer />
+     <header className='faixav'>
         <img className='logov' src='./images/IMG-20220418-WA0079_3.svg' />
         <div>
             <Link to='/'className="botoesv">Voltar</Link>
-            <Link to='/verpets'className="botoesu">Finalizar</Link>
+            <button onClick={salvarClick} className="botoesu">Finalizar</button>
         </div>
     </header>
 
@@ -20,61 +51,61 @@ export default function index() {
         <section className='bloco1v'>
             <div className='animalzinhov'>
                 <p className='letras'>Animal:</p>
-                <input className='primeirov'/>
+                <input className='primeirov' value={animal} onChange= {e => setAnimal(e.target.value)}/>
             </div>
             <div className='especiev'>
              <div>
                  <p className='letras'>Espécie:</p>
-                 <input className='segundov'/>
+                 <input className='segundov' value={especie} onChange= {e => setEspecie(e.target.value)}/>
              </div>
              <div className='testev'>
                  <p className='letras'>Gênero:</p>
-                 <input className='terceirov'/>
+                 <input className='terceirov' value={genero} onChange= {e => setGenero(e.target.value)}/>
              </div>
             </div>
 
             <div className='NOMEv'>
                 <div>
                     <p className='letras'>Nome:</p>
-                    <input className='quartov'/>
+                    <input className='quartov' value={nome} onChange= {e => setNome(e.target.value)}/>
                 </div>
                 <div className='testev'>
                     <p className='letras'>Idade:</p>
-                    <input className='quintov'/>
+                    <input className='quintov' value={idade} onChange= {e => setIdade(e.target.value)}/>
                 </div>
                </div>
 
                <div className='PESOv'>
                 <div>
                     <p className='letras'>Peso:</p>
-                    <input className='sextov'/>
+                    <input className='sextov' value={peso} onChange= {e => setPeso(e.target.value)}/>
                 </div>
                 <div className='testev'>
                     <p className='letras'>Altura:</p>
-                    <input className='setimov'/>
+                    <input className='setimov' value={altura} onChange= {e => setAltura(e.target.value)}/>
                 </div>
                </div>
 
                <div className='PhoneNumberv'>
                 <p className='letras'>Telefone para Contato:</p>
-                <input className='oitavov'/>
+                <input className='oitavov'  value={telefone} onChange= {e => setTelefone(e.target.value)} />
             </div>
 
             <div className='CEPv'>
                 <p className='letras'>Endereço:</p>
-                <input className='nonov'/>
+                <input className='nonov'  value={endereco} onChange= {e => setEndereco(e.target.value)}/>
             </div>   
         </section>
 
         <section className='bloco2v'>
             <div className='Cometariosv'>
                 <p className='letras'>Coméntarios Sobre (Doenças/alergias):</p>
-                <input className='decimov'/>
+                <input className='decimov'  value={comentario} onChange= {e => setComentario(e.target.value)}/>
             </div>
 
             <div className='ImagemPetv'>
                 <p className='letras'>Adicione Uma Imagem do Pet:</p>
-                <input type="file" className='onzev'/>
+                <input type="file" className='onzev'  />
             </div>
         </section>
 
