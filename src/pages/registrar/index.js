cb831d52ab70b5214110f06cdc0c5027859eea3a
juliana <julianafatiam5099@gmail.com>
2022-06-12 23:17:58 -0300

    registro

--- src/pages/registrar/index.js
+++ src/pages/registrar/index.js
@@ -12,6 +12,7 @@ export default function Index() {
     const [cpf, setCpf]= useState('');
     const [endereco, setEndereco]= useState('');
     const [senha, setSenha]= useState('');
+    const [senha2, setSenha2]= useState('');
     const [telefone, setTelefone]= useState('');
     const [sexo, setSexo]= useState('');
     const [email, setEmail]= useState('');
