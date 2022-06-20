bcf055192a32ef3cd170a2d1481afbbddd3f8e8e
Servulo <matheusservulo4@gmail.com>
2022-06-20 10:34:53 -0300

    registrar

--- src/pages/meuspets/index.js
+++ src/pages/meuspets/index.js
@@ -108,11 +108,11 @@ export default function Index() {
                   <p className='ejj'>ESPÉCIE:</p>
                   <p className='avejj'>{item.animal},{item.especie}</p>
                   <div className='femininojj' > 
-                  {item.genero  === 'MACHO' &&
+                  {item.genero  === 'Macho' &&
                             <img className='hominn' src="./images/homi.png" />
                         }
 
-                  {item.genero === 'FEMEA' &&
+                  {item.genero === 'Femea' &&
                           <img className='muienn' src="./images/mulhe.png" /> 
                         }
                   </div>
