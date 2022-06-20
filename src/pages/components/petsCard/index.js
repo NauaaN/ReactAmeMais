3636891887fa55c3ae285b7ac2d5692e5a14c529
nauan <nauan2006network@gmail.com>
2022-06-19 23:35:32 -0300

    aa

--- src/pages/components/petsCard/index.js
+++ src/pages/components/petsCard/index.js
@@ -47,11 +47,11 @@ export default function Index(props){
                   <p className='ejj'>ESPÉCIE:</p>
                   <p className='avejj'>{props.item.animal},{props.item.especie}</p>
                   <div className='femininojj' > 
-                  {props.item.genero  === 'MACHO' &&
+                  {props.item.genero  === 'macho' &&
                             <img className='hominn' src="./images/homi.png" />
                         }
 
-                  {props.item.genero === 'FEMEA' &&
+                  {props.item.genero === 'femea' &&
                           <img className='muienn' src="./images/mulhe.png" /> 
                         }
                   </div>
